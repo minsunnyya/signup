45fe8d1768a18068cf491f25b9db83221c16c6fe
minsunnyya <sun2547@naver.com>
2022-12-04 20:15:45 +0900

    feat : 회원가입 Encoding처리

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -1,27 +1,30 @@
 package com.example.demo.controller;
 
 import com.example.demo.domain.dto.UserJoinRequest;
+import com.example.demo.domain.dto.UserLoginRequest;
 import com.example.demo.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.aspectj.lang.annotation.Before;
+import org.apache.tomcat.util.descriptor.web.FragmentJarScannerCallback;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @WebMvcTest
 class UserControllerTest {
-
     @Autowired
     MockMvc mockMvc;
 
@@ -30,28 +33,33 @@ class UserControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+        String userName = "ageysdfse";
+        String password = "1234";
 
-//    @Test
-//    @DisplayName("회원가입 성공")
-//    void join() throws Exception {
-//        String userName = "minsun";
-//        String password = "1234";
-//        mockMvc.perform(post("/api/v1/users/join")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
-//                .andDo(print())
-//                .andExpect(status().isOk());
-//    }
-//    @Test
-//    @DisplayName("회원가입 실패 - userName중복")
-//    void join_fail() throws Exception {
-//        String userName = "minsun";
-//        String password = "1234";
-//        mockMvc.perform(post("/api/v1/users/join")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName,password))))
-//                .andDo(print())
-//                .andExpect(status().isConflict());
-//
-//    }
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
+            .andDo(print())
+            .andExpect( status().isOk());
+    }
+    @Test
+    @DisplayName("회원가입 실패")
+    void join_fail() throws Exception {
+        String userName = "Minsun";
+        String password = "1234";
+
+        when(userService.join(any(),any()))
+                .thenThrow(new RuntimeException("해당 userId가 중복됩니다"));
+
+    mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName,password))))
+            .andDo(print())
+            .andExpect(status().isConflict());
+    }
+
+
 }
